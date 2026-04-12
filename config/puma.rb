097ff8666824a5717c5dc4a4c83a5032a4c1a53a
jeffreyleungch <jeffreyleungch@gmail.com>
2026-04-12 14:50:09 +0000

    debug4

--- config/puma.rb
+++ config/puma.rb
@@ -1,8 +1,17 @@
-workers Integer(ENV.fetch('WEB_CONCURRENCY', 1))
+worker_count = Integer(ENV.fetch('WEB_CONCURRENCY', 0))
+workers worker_count if worker_count.positive?
 threads_count = Integer(ENV.fetch('RAILS_MAX_THREADS', 5))
 threads threads_count, threads_count
 
 port ENV.fetch('PORT', 9292)
 environment ENV.fetch('RACK_ENV', 'development')
+
+if worker_count.positive?
+  preload_app!
+
+  on_worker_boot do
+    ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
+  end
+end
 
 plugin :tmp_restart
