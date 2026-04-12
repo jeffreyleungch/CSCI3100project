ad3ccb730c05904f36df1fd3f7551951603d5d58
jeffreyleungch <jeffreyleungch@gmail.com>
2026-04-12 14:29:34 +0000

    debug3

--- config/puma.rb
+++ config/puma.rb
@@ -5,6 +5,4 @@ threads threads_count, threads_count
 port ENV.fetch('PORT', 9292)
 environment ENV.fetch('RACK_ENV', 'development')
 
-bind "tcp://0.0.0.0:#{ENV.fetch('PORT', 9292)}"
-
 plugin :tmp_restart
